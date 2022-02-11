aeca47858ccc769a3eeabba875397da14b974cee
Charlie Cox <phortysiks@me.com>
2022-02-11 10:44:08 +0000

    Updating of display logic completed. Still to work on multi-zero input at start

--- index.js
+++ index.js
@@ -27,3 +27,59 @@ const operate = function(operator, num1, num2) {
         return "Error"
     }
 };
+
+let working = document.querySelector("#working");
+let displayText = document.querySelector("#main-display");
+let displayArr = [];
+let firstArgument = null;
+let secondArgument = null;
+let operatingArgument = null;
+
+
+function updateDisplay(e) {
+    displayArr.push(e.target.textContent);
+    let updatedDisplay = displayArr.join('');
+    displayText.textContent = updatedDisplay;
+};
+
+function deleteCharacter() {
+    displayArr.pop();
+    if (displayArr.length == 0) {
+        reset();
+    } else if (displayArr.length == 1 && displayArr[0] == "-") {
+        reset();
+    } else {
+        let updatedDisplay = displayArr.join('');
+        displayText.textContent = updatedDisplay;
+    }
+};
+
+function reset() {
+    displayArr = [];
+    displayText.textContent = 0;
+}
+
+function toggleSign() {
+    if (displayArr[0] == "-") {
+        displayArr.shift();
+    } else {
+        displayArr.unshift("-");
+    }
+    let updatedDisplay = displayArr.join('');
+    displayText.textContent = updatedDisplay;
+}
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(number => {number.addEventListener('click', updateDisplay)});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {operator.addEventListener('click', setOperator)});
+
+const del = document.querySelector("#del");
+del.onclick = deleteCharacter;
+
+const clear = document.querySelector("#clear");
+clear.onclick = reset;
+
+const posOrNeg = document.querySelector("#plus-minus");
+posOrNeg.onclick = toggleSign;
